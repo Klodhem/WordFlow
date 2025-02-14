f2f7b5659677bbbf85d2332fb00d4d6e9594de07
Klodhem <wilmer.hemilton@gmail.com>
2025-02-14 12:00:56 +0500

    Add Exceptions and ExceptionHandler

--- backend/src/main/java/git/klodhem/backend/config/FileExceptionHandler.java
+++ backend/src/main/java/git/klodhem/backend/config/FileExceptionHandler.java
@@ -2,6 +2,8 @@ package git.klodhem.backend.config;
 
 import git.klodhem.backend.exception.ErrorResponse;
 import git.klodhem.backend.exception.FileUploadException;
+import git.klodhem.backend.exception.UserLoginException;
+import git.klodhem.backend.exception.UserRegistrationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,14 +15,11 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class FileExceptionHandler {
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<ErrorResponse> handler(MaxUploadSizeExceededException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(FileUploadException.class)
-    public ResponseEntity<ErrorResponse> handler(FileUploadException ex) {
+    @ExceptionHandler({UserLoginException.class,
+            UserRegistrationException.class,
+            FileUploadException.class,
+            MaxUploadSizeExceededException.class})
+    public ResponseEntity<ErrorResponse> handler(RuntimeException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
