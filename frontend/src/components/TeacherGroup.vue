24eff93cd5d728b3c144d95c661182505465242c
Klodhem <wilmer.hemilton@gmail.com>
2025-07-22 22:21:43 +0500

    Updated API route for group video

--- frontend/src/components/TeacherGroup.vue
+++ frontend/src/components/TeacherGroup.vue
@@ -42,7 +42,7 @@ const getStudentsAndVideosGroup = async () => {
   try {
     let response = await apiClient.get(`/groups/${groupId}/students`)
     students.value = response.data
-    response = await apiClient.get(`/videos/group/${groupId}`)
+    response = await apiClient.get(`/groups/${groupId}/videos`)
     videos.value = response.data
     selectedVideosTeacher.value = response.data
   } catch (err) {
